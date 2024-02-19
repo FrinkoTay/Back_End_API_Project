38fac139ada7c8411ed743937ca12c6672971663
Frankie Taylor <147100290+FrinkoTay@users.noreply.github.com>
2024-02-19 13:40:19 +0000

    Update controller.js to remove test console log

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,7 +1,6 @@
 const { selectAllTopics } = require('../models/model')
 
 exports.getAllTopics = (req, res, next) => {
-    console.log('before model')
     selectAllTopics()
     .then((topics) => {
         res.status(200).send(topics)
